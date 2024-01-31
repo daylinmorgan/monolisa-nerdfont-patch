680f3b6917fe12ff2adeb676dda0590458ac4f21
Daylin Morgan <daylinmorgan@gmail.com>
2024-01-30 20:33:49 -0600

    build package

--- flake.nix
+++ flake.nix
@@ -12,6 +12,36 @@
       ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
       (system: f nixpkgs.legacyPackages.${system});
   in {
+    packages = forAllSystems (
+      pkgs:
+      with pkgs; {
+        default = stdenv.mkDerivation {
+        name = "monolisa-nerdfont-patch";
+        src = ./.;
+        nativeBuildInputs = [ makeWrapper ];
+        buildInputs = [
+          fontforge
+          python3
+        ];
+        unpackPhase = ":";
+        buildPhase = ":";
+        installPhase = ''
+          mkdir -p $out/bin
+          install -m755 -D ${./patch-monolisa} $out/bin/monolisa-nerdfont-patch
+          install -m755 -D ${./font-patcher} $out/bin/font-patcher
+          cp -r ${./bin} $out/bin/bin
+          cp -r ${./src} $out/bin/src
+        '';
+        postFixup = ''
+          wrapProgram $out/bin/monolisa-nerdfont-patch \
+            --set PATH ${lib.makeBinPath [
+              fontforge
+            ]}
+          '';
+      };
+    }
+      );
+
     devShells = forAllSystems (
       pkgs:
         with pkgs; {
